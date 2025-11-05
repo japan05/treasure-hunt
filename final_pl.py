f659934c13079af982f3155572b3fb462a10535d
Kiri-Sigh <sigh.tul.1@gmail.com>
2025-11-05 23:48:14 +0700

    Update final_pl.py

--- final_pl.py
+++ final_pl.py
@@ -3,7 +3,10 @@ from pyswip import Prolog
 prolog = Prolog()
 prolog.consult("final_w.pl")
 
-result = list(prolog.query("run(A)"))
+result = list(prolog.query("run(Step,Score)"))
 if result:
-    path = result[0]['A']
-    print("A:", path)
+    path = result[0]['Step']
+    print("Step:", path)
+    path = result[0]['Score']
+    print("Score:", path)
+
